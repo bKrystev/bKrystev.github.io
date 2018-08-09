88bb32b244e7f9eeb3c34ab3a28fb742dfc5b430
bKrystev <bobislavbg@gmail.com>
2018-08-09 20:45:19 +0300

    fixed route for media query project

--- MediaQuery/Script.js
+++ MediaQuery/Script.js
@@ -2,17 +2,17 @@ let myWindow;
 
 function smallScreen() {
    
-   myWindow = window.open("file:///E:/VS%20saves/MediaQuery/index.html",
+   myWindow = window.open("html3.html",
         "popup", "width=250px,height=450px,left=200,top=80")
 }
 function landscape() {
     myWindow.close();
-   myWindow = window.open("file:///E:/VS%20saves/MediaQuery/index.html",
+   myWindow = window.open("html3.html",
         "popup", "width=450px,height=250px,left=200,top=90,")
 }
 function tabletScreen() {
     myWindow.close();
-    myWindow = window.open("file:///E:/VS%20saves/MediaQuery/index.html",
+    myWindow = window.open("html3.html",
         "popup", "width=700,height=400,left=200,top=100")
    
 }
