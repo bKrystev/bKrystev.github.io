4ee33f7faaf43af971670f11a3ef84dc1c340e7c
bKrystev <bobislavbg@gmail.com>
2018-08-09 20:47:26 +0300

    tried to fix route

--- MediaQuery/Script.js
+++ MediaQuery/Script.js
@@ -2,17 +2,17 @@ let myWindow;
 
 function smallScreen() {
    
-   myWindow = window.open("html3.html",
+    myWindow = window.open("MediaQuery/html3.html",
         "popup", "width=250px,height=450px,left=200,top=80")
 }
 function landscape() {
     myWindow.close();
-   myWindow = window.open("html3.html",
+    myWindow = window.open("MediaQuery/html3.html",
         "popup", "width=450px,height=250px,left=200,top=90,")
 }
 function tabletScreen() {
     myWindow.close();
-    myWindow = window.open("html3.html",
+    myWindow = window.open("MediaQuery/html3.html",
         "popup", "width=700,height=400,left=200,top=100")
    
 }
